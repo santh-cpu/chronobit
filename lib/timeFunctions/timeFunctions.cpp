547ba885c28fd9697fdf2ba21c274c508ed7c5d3
beppvis <visruthvijind@gmail.com>
2025-10-03 12:03:44 +0530

    works

---
+++ lib/timeFunctions/timeFunctions.cpp
@@ -0,0 +1,42 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <FastLED.h>
+#include <time.h>
+#include "Arduino.h"
+
+#define NTP_SERVER "pool.ntp.org"
+#define GMT_OFFSET_SEC 19800  // GMT+5:30 for India
+#define DAYLIGHT_OFFSET_SEC 0
+
+struct tm timeinfo;
+bool timeValid = false;
+bool colonVisible = true;
+unsigned long lastColonToggle = 0;
+unsigned long lastTimeSync = 0;
+
+
+void setupTime() {
+  configTime(GMT_OFFSET_SEC, DAYLIGHT_OFFSET_SEC, NTP_SERVER);
+  Serial.print("Waiting for NTP time sync");
+  int attempts = 0;
+  while (!getLocalTime(&timeinfo) && attempts < 10) {
+    Serial.print(".");
+    delay(1000);
+    attempts++;
+  }
+  
+  if (getLocalTime(&timeinfo)) {
+    Serial.println("\nTime synchronized!");
+    timeValid = true;
+    lastTimeSync = millis();
+  } else {
+    Serial.println("\nFailed to obtain time");
+    timeValid = false;
+  }
+}
+
+struct tm getTime(){
+  return timeinfo;
+}
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <FastLED.h>
+#include <time.h>
 #include "notification.cpp"
+#include "timeFunctions.cpp"
 #include "font.h"
 #include "math.h"
 
@@ -19,6 +21,7 @@ CRGB leds[NUM_LEDS];
 
 const char* ssid     = "SamsungA22";
 const char* password = "passwrdhehe";
+extern unsigned long lastTimeSync;
 
 WebServer server(80);
 
@@ -39,13 +42,80 @@ void handleNotify() {
   }
 }
 
-
-
-
 void handleRoot() {
-  server.send(200, "text/html",
-              "<h2>ESP32 Notification Server</h2>"
-              "<p>Use <b>/notify?msg=call</b>, <b>msg</b>, or <b>alarm</b></p>");
+  String html = "<!DOCTYPE html><html><head><title>ESP32 Control</title></head><body>";
+  html += "<h2>ESP32 Notification & Clock</h2>";
+
+  // Clock controls
+  html += "<h3>Clock</h3>";
+  html += "<a href=\"/clock\">Show Clock</a><br>";
+
+  // Stopwatch controls
+  html += "<h3>Stopwatch</h3>";
+  html += "<a href=\"/stopwatch/start\">Start Stopwatch</a><br>";
+  html += "<a href=\"/stopwatch/stop\">Stop Stopwatch</a><br>";
+  html += "<a href=\"/stopwatch/reset\">Reset Stopwatch</a><br>";
+
+  // Timer controls
+  html += "<h3>Timer</h3>";
+  html += "<form action=\"/timer/start\" method=\"get\">";
+  html += "Duration (sec): <input type=\"text\" name=\"sec\">";
+  html += "<input type=\"submit\" value=\"Start Timer\">";
+  html += "</form>";
+  html += "<a href=\"/timer/stop\">Stop Timer</a><br>";
+
+  // Notification test
+  html += "<h3>Notifications</h3>";
+  html += "<a href=\"/notify?msg=call\">Notify: Call</a><br>";
+  html += "<a href=\"/notify?msg=msg\">Notify: Message</a><br>";
+  html += "<a href=\"/notify?msg=alarm\">Notify: Alarm</a><br>";
+
+  html += "</body></html>";
+  server.send(200, "text/html", html);
+}
+
+void handleClock() {
+  server.send(200, "text/plain", "Clock mode activated");
+}
+
+unsigned long stopwatchStart = 0;
+bool stopwatchRunning = false;
+
+void handleStopwatchStart() {
+  stopwatchStart = millis();
+  stopwatchRunning = true;
+  server.send(200, "text/plain", "Stopwatch started!");
+}
+
+void handleStopwatchStop() {
+  stopwatchRunning = false;
+  server.send(200, "text/plain", "Stopwatch stopped!");
+}
+
+void handleStopwatchReset() {
+  stopwatchStart = millis();
+  stopwatchRunning = false;
+  server.send(200, "text/plain", "Stopwatch reset!");
+}
+
+unsigned long timerDuration = 0;
+unsigned long timerStart = 0;
+bool timerRunning = false;
+
+void handleTimerStart() {
+  if (server.hasArg("sec")) {
+    timerDuration = server.arg("sec").toInt();
+    timerStart = millis();
+    timerRunning = true;
+    server.send(200, "text/plain", "Timer started for " + String(timerDuration) + " sec");
+  } else {
+    server.send(400, "text/plain", "Missing ?sec parameter");
+  }
+}
+
+void handleTimerStop() {
+  timerRunning = false;
+  server.send(200, "text/plain", "Timer stopped");
 }
 
 
@@ -66,8 +136,6 @@ void drawNum(int num, int start_x,int start_y,CRGB color){
 	  leds[XY2Index(grid_x, grid_y)] = color;
 	}
       }
-
-
     }
   }
 
@@ -102,20 +170,42 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+  setupTime();
   Serial.println("\nWiFi connected!");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
   server.on("/",handleRoot);
   server.on("/notify",handleNotify);
+  
+server.on("/clock", handleClock);
+
+server.on("/stopwatch/start", handleStopwatchStart);
+server.on("/stopwatch/stop", handleStopwatchStop);
+server.on("/stopwatch/reset", handleStopwatchReset);
+
+server.on("/timer/start", handleTimerStart);
+server.on("/timer/stop", handleTimerStop);
   server.begin();
   Serial.println("\nWeb Server Started! \n");
 }
 
 int iter = 0;
+
 void loop() {
-  unsigned int time[] = {0,0};
+ 
+  if (millis() - lastTimeSync > 3600000) {  
+    setupTime();
+  }
+  
+  if (!getLocalTime(&timeinfo)) {
+    timeValid = false;
+  } else {
+    timeValid = true;
+  }
+  
+  struct tm time = getTime();
   server.handleClient();
-  displayTime(time[0],time[1],CRGB::Blue, CRGB::Green);
+  displayTime(time.tm_hour,time.tm_min,CRGB::Blue, CRGB::Green);
   FastLED.show();
   delay(500);
   FastLED.clear();
