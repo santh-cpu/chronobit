604bb6aa85b39da27773cfcf71d49633bcfb639b
beppvis <visruthvijind@gmail.com>
2025-10-03 20:19:10 +0530

    Phase 1
    works3

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # ChronoBit
-----
 Choronobit aims to be not just a digital clock, but helps the user stay focused on their task without having the worry of missing out on important notifications, and work towards achieving their goals.
 
 
-## How to run stuff
-----
+## How to execute 
 1) Install platform io cli
 2) To compile
 	```
@@ -15,25 +13,9 @@ Choronobit aims to be not just a digital clock, but helps the user stay focused 
 	pio run --target upload
 	```
 4) To access the serial monitor
-```
+	```
 	pio run --target monitor
 	```
-
-
-
-## Tasks
-----
-- [ ] create digital way of representing the information
-	- [ ] create time functions 
-		- [ ] currentTime() 
-	- [ ] work out how to draw a desire image    
-		- [ ] Dynamically render images
-		- [ ]  Design content disply 
-		- [ ] Make it dynamically work with given data values
-
-- [ ] User connectivity to device
-	- [ ] Write backend code to connect to the internet
-	- [ ] Write an app, to read user notification and send it to the device
 
 
 
--- lib/notifications/notification.cpp
+++ lib/notifications/notification.cpp
@@ -13,15 +13,6 @@
 #define IMAGE_HEIGHT 8
 #define NUM_LEDS (IMAGE_WIDTH * IMAGE_HEIGHT)
 
-
-const long ANIM_INTERVAL = 100;  // ms per pixel
-
-unsigned long prevMillis = 0;
-int animStep = 0;
-const int TOTAL_STEPS = (IMAGE_WIDTH + IMAGE_HEIGHT) * 2 - 4;  // total pixels around border
-
-void drawOutlineStep(CRGB* leds,int step,CRGB color); 
-
 int XY2Index(int x,int y){
   return (7-y)+(8*x);
 }
@@ -36,24 +27,6 @@ void flashColor(CRGB color,CRGB* leds, int flashes = 3, int delayMs = 1000) {
       delay(delayMs);
     }
 }
-
-void drawOutlineStep(CRGB* leds,int step,CRGB color) {
-  if (step < IMAGE_WIDTH) {                 // Top border
-    leds[XY2Index(step, 0)] = color;
-  } 
-  else if (step < IMAGE_WIDTH + IMAGE_HEIGHT - 1) {  // Right border
-    leds[XY2Index(IMAGE_WIDTH-1, step-IMAGE_WIDTH)] = color;
-  } 
-  else if (step < IMAGE_WIDTH*2 + IMAGE_HEIGHT - 2) { // Bottom border
-    leds[XY2Index(IMAGE_WIDTH*2+IMAGE_HEIGHT-3-step, IMAGE_HEIGHT-1)] = color;
-  } 
-  else if (step < (IMAGE_WIDTH*2) + (IMAGE_HEIGHT*2) - 4) { // Left border
-    leds[XY2Index(0,IMAGE_WIDTH*2+IMAGE_HEIGHT*2-5)] = color;
-  }
- 
-}
-
-
 void showNotification(String msg,CRGB* leds) {
   msg.toLowerCase();
   if (msg == "call") {
